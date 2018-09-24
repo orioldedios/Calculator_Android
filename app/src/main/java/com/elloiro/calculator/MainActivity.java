1a4fe1148e6a862116d4849f5705a5cab0fc96da
Oriol de Dios Miranda <orioldedios@gmail.com>
2018-09-24 20:52:33 +0200

    Basic calculator done

--- app/src/main/java/com/elloiro/calculator/MainActivity.java
+++ app/src/main/java/com/elloiro/calculator/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
 
     private String num = "";
     private TextView num_view;
+
     private enum operations
     {
         PLUS,
@@ -21,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
     private operations operation_saved = operations.NONE;
     private float first_number_saved = 0.0f;
     private  boolean operand_clicked_before = false;
-
+    private  boolean equal_button_clicked_before = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,59 +32,94 @@ public class MainActivity extends AppCompatActivity {
         num_view.setText(num);
     }
 
-    public void OnClickDigit(View view)
-    {
-        if(operand_clicked_before)
+    public void OnClickDigit(View view) {
+        if (num_view.getText().toString() != "ERROR")
         {
-            num_view.setText("");
-            operand_clicked_before = false;
-            num = "";
+            if (operand_clicked_before) {
+                num_view.setText("");
+                operand_clicked_before = false;
+                num = "";
+            }
+            Button b = (Button) view;
+            num += b.getText().toString().charAt(0);
+            num_view.setText(num);
         }
-        Button b = (Button)view;
-        num += b.getText().toString().charAt(0);
-        num_view.setText(num);
     }
-
     public void OnClickOperand (View view)
     {
-        Button b = (Button)view;
-        char switch_case = b.getTag().toString().charAt(0);
-        switch (switch_case)
+        if (equal_button_clicked_before || operation_saved != operations.NONE || num == "")
         {
-            case 'p':
-                operation_saved = operations.PLUS;
-                break;
-            case  'r':
-                operation_saved = operations.MINUS;
-                break;
-            case 'm':
-                operation_saved = operations.MULTIPLICATION;
-                break;
-            case 'd':
-                operation_saved = operations.DIVISION;
-                break;
+            num_view.setText("ERROR");
+            num = "";
         }
-        first_number_saved = Float.parseFloat(num);
-        operand_clicked_before = true;
+        else
+        {
+            Button b = (Button)view;
+            char switch_case = b.getTag().toString().charAt(0);
+            switch (switch_case)
+            {
+                case 'p':
+                    operation_saved = operations.PLUS;
+                    break;
+                case  'r':
+                    operation_saved = operations.MINUS;
+                    break;
+                case 'm':
+                    operation_saved = operations.MULTIPLICATION;
+                    break;
+                case 'd':
+                    operation_saved = operations.DIVISION;
+                    break;
+            }
+            first_number_saved = Float.parseFloat(num);
+            operand_clicked_before = true;
+            num = "";
+        }
     }
 
     public void OnClickEqual (View view)
     {
-       float second_number_saved = Float.parseFloat(num);
-        switch (operation_saved)
+        if (num_view.getText().toString() != "ERROR" && operation_saved != operations.NONE)
         {
-            case NONE:
-                break;
-            case PLUS:
-                num_view.setText(Float.toString(first_number_saved + second_number_saved));
-                break;
-            case MINUS:
-                break;
-            case DIVISION:
-                break;
-            case MULTIPLICATION:
-                break;
+            Operate();
+            operand_clicked_before = false;
+            equal_button_clicked_before = true;
         }
-        operand_clicked_before = false;
     }
+
+    public void OnClickDot(View view)
+    {
+
+    }
+
+    public void OnClickCE(View view)
+    {
+        num = "";
+        operation_saved = operations.NONE;
+        first_number_saved = 0.0f;
+        operand_clicked_before = false;
+        equal_button_clicked_before = false;
+        num_view.setText(num);
+    }
+
+    public  void Operate ()
+    {
+        float second_number_saved = Float.parseFloat(num);
+            switch (operation_saved) {
+                case NONE:
+                    break;
+                case PLUS:
+                    num_view.setText(Float.toString(first_number_saved + second_number_saved));
+                    break;
+                case MINUS:
+                    num_view.setText(Float.toString(first_number_saved - second_number_saved));
+                    break;
+                case DIVISION:
+                    num_view.setText(Float.toString(first_number_saved / second_number_saved));
+                    break;
+                case MULTIPLICATION:
+                    num_view.setText(Float.toString(first_number_saved * second_number_saved));
+                    break;
+            }
+        }
 }
